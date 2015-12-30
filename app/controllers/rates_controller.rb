25a8cc655f52d2557bfa3b53e8f970ddec5969df
Tim Chai <timothy.y.chai@gmail.com>
2015-12-29 22:14:58 -0800

    api

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -7,7 +7,6 @@ class RatesController < ApplicationController
     Rubillow.configure do |configuration|
     configuration.zwsid = "X1-ZWz1f3hwtklxjf_3csw9"
     end
-
     @rates = Rubillow::Mortgage.rate_summary.today
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   resources :loans
   resources :rates
+  resources :applications
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
