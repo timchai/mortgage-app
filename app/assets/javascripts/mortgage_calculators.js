9e47a3dcb28e15a0378764cd05afa4b4de2402ba
Tim Chai <timothy.y.chai@gmail.com>
2016-01-06 22:14:57 -0800

    calculator

--- app/assets/javascripts/mortgage_calculators.js
+++ app/assets/javascripts/mortgage_calculators.js
@@ -1,7 +1,20 @@
-var loanprincipal = document.mortgagecalc.loan.value;
-var months = document.mortgagecalc.years.value * 12;
-var interest = document.mortgagecalc.rate.value / 1200;
+// (function () {
+//   "use strict";
 
-function myPayment() {
-  document.getElementById('monthlyPayment').innerHTML = 'Your monthly payment will be ' + '$' + (loanprincipal * interest / (1 - (Math.pow(1/(1 + interest), months)))).toFixed(2)+'.';
-}
+//   angular.module("app").controller("")
+// })
+
+$( document ).ready(function() {
+
+  $( "#calculate" ).click(function() {
+    var loanprincipal = document.getElementById("loansize").value;
+    var months = document.getElementById("years").value * 12;
+    var interest = document.getElementById("interest").value / 1200;
+    console.log(loanprincipal);
+
+    document.getElementById('monthlyPayment').innerHTML = 'Your monthly payment will be ' + '$' + (loanprincipal * interest / (1 - (Math.pow(1/(1 + interest), months)))).toFixed(2)+'.';
+  });
+});
+
+// function myPayment() {
+// }
