b7aa904eefab2b04faf39709a7547a26d58c1744
Tim Chai <timothy.y.chai@gmail.com>
2016-01-15 15:30:34 -0800

    improve calculations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,25 +1,25 @@
 class Application < ActiveRecord::Base
 
-  def max_loan(rates, year_fixed)
-    return ((((self.income / 12) * 0.28) / (rates[year_fixed].to_i * 110)) * 100000)
+  def max_loan(rates)
+    return ((((self.income / 12) * 0.28) / (rates[:thiry_year_fixed].to_i * 200)) * 100000)
   end
 
-  def interest_evaluation (rates, all_rates)
+  def interest_evaluation(rates, all_rates)
     if self.fico_score >= 750 && self.fico_score <= 850
       return rates[all_rates]
     elsif self.fico_score < 750 && self.fico_score >= 700
-      return ((rates[all_rates]).to_i + 0.31).round(2)
+      return ((rates[all_rates]).to_f * 1.21)
     elsif self.fico_score < 700 && self.fico_score >= 650
-      return ((rates[all_rates]).to_i + 0.52).round(2)
+      return ((rates[all_rates]).to_f * 1.40)
     elsif self.fico_score < 650 && self.fico_score >= 600
-      return ((rates[all_rates]).to_i + 1.25).round(2)
+      return ((rates[all_rates]).to_f * 1.65)
     elsif self.fico_score < 600 && self.fico_score >= 550
-      return ((rates[all_rates]).to_i + 1.55).round(2)
+      return ((rates[all_rates]).to_f * 1.85)
     end
   end
 
-  # def monthly_payment_30fixed
-  #   max_loan
+  # def monthly_payment_30(rates)
+    # return max_loan * ((rates[:thiry_year_fixed].to_i)/(12*100)) / ((1 - ((1 + (rates[:thiry_year_fixed].to_i)/(12*100))**-360)))
   # end
 
 end
